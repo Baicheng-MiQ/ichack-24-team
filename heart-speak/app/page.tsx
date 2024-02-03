4ea9ac822ea48e29777951a14c48fc1e69a1a896
Aaron Thomas <at2222@ic.ac.uk>
2024-02-03 17:18:20 +0000

    (Update heart-speak directory) - initialized Next Project, with TypeScript and TailWindCSS

---
+++ heart-speak/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import LeftNavbar from "./components/LeftNavbar";
+
+export default function Home() {
+  return (
+    <>
+      <LeftNavbar />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        hello
+      </main>
+    </>
+  );
+}
